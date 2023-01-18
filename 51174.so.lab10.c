04d573a93deae700dea43a87730c969d0122b015
marianstrilets <marianstrilets@gmail.com>
2023-01-18 15:52:13 +0100

    Update 51174.so.lab10.c

--- 51174.so.lab10.c
+++ 51174.so.lab10.c
@@ -2,6 +2,7 @@
 // Marian Strilets
 // sm51174@zut.edu.pl
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,11 +27,11 @@ struct thread_args {
 
 double wallisa(int start, int end ){
     double result = 1;
-    for (int i = start; i <= end; i++) {
+    for (int i = start; i < end; i++) {
         result *= ((double) (2*i)) / ((double) (2*i - 1));
         result *= ((double) (2*i)) / ((double) (2*i + 1));
     }
-    result*=2;
+    //result*=2;
     return result;
 }
 
@@ -47,21 +48,33 @@ void *program2pthread(void *args) {
     struct thread_args *thread_args = (struct thread_args *) args;
     pthread_t myself = pthread_self();
 
-    int thread_id = thread_args->thread_id;
+    
+    //int thread_id = thread_args->thread_id;
     int start_index = thread_args->start_index;
     int end_index = thread_args->end_index;
-
+    //double thread_product =1;
 
     //printf("Wątek %d rozpoczyna pracę na zakresie [%d, %d]\n", thread_id, start_index, end_index);
     printf("Thread #%lx size=%d first=%d\n", (unsigned long int)myself, end_index - start_index, start_index);
 
-    //double thread_product = 1;
-    //thread_product = wallisa(start_index, end_index);
-    global_product =  wallisa(start_index, end_index);
-    printf("Thread #%lx prod=%.20f\n", (unsigned long int)myself,global_product); //thread_product);
+    double thredProduct = 0;
+    pthread_mutex_lock(&mutex);    
 
-    //function_mutex(thread_product);
-    function_mutex(global_product);
+    thredProduct =  wallisa(start_index, end_index);
+    //global_product =  wallisa(start_index, end_index);
+    global_product *= thredProduct;
+
+    //function_mutex(global_product);
+    //pthread_mutex_lock(&mutex);
+
+    //global_product *= thread_product;
+
+    //pthread_mutex_unlock(&mutex);
+
+    //pthread_exit(NULL);
+    pthread_mutex_unlock(&mutex);
+    printf("Thread #%lx prod=%.20f\n", (unsigned long int)myself,thredProduct);//global_product); 
+    pthread_exit(NULL);
 }
 
 
@@ -102,6 +115,7 @@ int main(int argc, char *argv[]) {
         thread_args[i].end_index = thread_args[i].start_index + segmentSize-1; 
         if (i < remainder) 
             thread_args[i].end_index++;  
+        
         pthread_create(&threads[i], NULL, program2pthread, (void *) &thread_args[i]); 
     }
 
@@ -110,7 +124,7 @@ int main(int argc, char *argv[]) {
 
     clock_gettime(CLOCK_REALTIME, &stop);
     double time_product = (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / (double)1000000000L;
-    printf("w/Thread: PI=%.20f time=%lf\n", global_product, time_product);
+    printf("w/Thread: PI=%.20f time=%lf\n", global_product*2, time_product);
 
 
     //-------------------------------------------------- wo/Threads -------------------------------------------
@@ -119,7 +133,7 @@ int main(int argc, char *argv[]) {
 
     clock_gettime(CLOCK_REALTIME, &stop);
     time_product = (stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / (double)1000000000L;
-    printf("wo/Thread: PI=%.20f time=%lf\n", global_product, time_product);
+    printf("wo/Thread: PI=%.20f time=%lf\n", global_product*2, time_product);
 
     return 0;
 }
